a2fdc5fded0a53e7aad97de4da7282869602a4cb
Evgeny Poberezkin <e.poberezkin@me.com>
2016-03-27 16:08:37 +0100

    step 4 - send message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict';
 
 var db = new Firebase('https://chattinghere.firebaseio.com/');
+var messages;
+var currentChannel;
 
 $('#create-channel-btn').on('click', function() {
     db.child('channels').push({
@@ -19,10 +21,28 @@ db.child('channels').on('child_added', function (snapshot) {
     var ch = $('#channel-item-template').clone();
     ch.removeClass('hidden');
     ch.removeAttr('id');
-    for (var prop in channel) {
-        $('[data=' + prop + ']', ch).html(channel[prop]);
-    }
+    showData(ch, channel);
     $('#channel-list').append(ch);
+
+    ch.on('click', function() {
+        currentChannel = channel;
+        messages = db.child('messages/' + channel.key);
+        showData($('#channel-info'), channel);
+    });
 });
+
+$('#send-message').on('click', function() {
+    messages.push({
+        text: $('#message-text').val(),
+        timestamp: Date.now()
+    });
+});
+
+
+function showData($el, data) {
+    for (var prop in data) {
+        $('[data=' + prop + ']', $el).html(data[prop]);
+    }
+}
 
 })();
